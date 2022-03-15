4ebc638c8e5920fbb49b0a5071b83e0e9d5484d3
gio_barranco <sandiego_94@hotmail.com>
2022-03-15 17:45:45 -0600

    created readme and placed video link/updated html

--- Develop/utils/generateHTML.js
+++ Develop/utils/generateHTML.js
@@ -6,25 +6,44 @@ function generateHTML(employeesArray){
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>Team Profile</title>
     </head>
     <body>
-        <h1>My Team</h1>
+    <div class="container m-2">
+    <h1 class="text-white bg-primary m-3">My Team</h1>
         <!--Looping through all items in an array -->
         ${employeesArray.map(employee =>{
             console.log("Each Array Item" , employee); 
             if(employee.getRole() === "Manager"){
-                return `<div>
+                return `<div class="card">
                 <h1>${employee.name}</h1>
-                <h2>Manager </h2>
+                <h2>Manager</h2>
                 <p>Email: ${employee.email}</p>
                 <p>OfficeNumber: ${employee.officeNumber}</p>
-                </div>`
+                </div>
+                <br />`
             }
-            else{
-                return `<h3> createother cards </h3>`
+            else if(employee.getRole() === "Engineer"){
+                return `<div class="card">
+                <h1>${employee.name}</h1>
+                <h2>Engineer</h2>
+                <p>Email: ${employee.email}</p>
+                <p>Github Username: http://github.com/${employee.getGithub()}</p>
+                </div>
+                <br />`
+            }
+            else if(employee.getRole() === "Intern"){
+                return `<div class="card">
+                <h1>${employee.name}</h1>
+                <h2>Intern</h2>
+                <p>Email: ${employee.email}</p>
+                <p>School Name: ${employee.school}</p>
+                </div>
+                <br />`
             }
         })}
+        </div>
     </body>
     </html>`;
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # team-profile-generator
+
+## Description
+The team profile generator allows for the user to create cards for an Intern, Employee and Mananger. The user is prompted to type certain characteristics based on who they choose. Once they are done an html document is created where it can be opened in a browser and the user can see a web page with their chosen team and their characteristics.
+
+
+## Video Link: https://drive.google.com/file/d/1-VewYkEHHJGlkOdz_cTOQD6aVSRnkd47/view
